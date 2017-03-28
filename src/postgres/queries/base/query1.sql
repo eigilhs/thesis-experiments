28e2b7230f5b89d03c02be20da2442cc99ceec7a
Eigil Skjæveland <eigil@skjaeve.land>
2017-03-28 12:53:28 +0200

    Fix pg base queries

---
+++ src/postgres/queries/base/query1.sql
@@ -0,0 +1,19 @@
+SELECT
+    name,
+    avg(outcome::int) AS rate,
+    count(*) AS total
+FROM events
+JOIN qualifiers
+  ON events.id = qualifiers.event_id
+JOIN players
+  ON player_id = players.id
+JOIN matches
+  ON match_id = matches.id
+ AND competition_id = 90
+ AND season_id = 2016
+WHERE type_id = 1 AND qualifier_id = 1
+GROUP BY player_id, name
+HAVING count(outcome) > 100
+ORDER BY rate DESC
+NULLS LAST
+LIMIT 10;
