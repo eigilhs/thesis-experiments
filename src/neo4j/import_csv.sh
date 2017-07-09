a4231db0103e2396fb2548b319c3f4aa8622c2d5
Eigil Skjæveland <eigil@skjaeve.land>
2017-07-10 01:21:30 +0200

    Temporary fix to import_csv.

--- src/neo4j/import_csv.sh
+++ src/neo4j/import_csv.sh
@@ -5,20 +5,21 @@ $NEO4J_DIR/bin/neo4j-import --stacktrace true --multiline-fields true \
   --id-type string \
   --skip-duplicate-nodes true \
   --bad-tolerance 100000 \
-  --nodes:Player /tmp/basecsv/Player.csv \
-  --nodes:Team /tmp/basecsv/Team.csv \
-  --nodes:Competition /tmp/basecsv/Competition.csv \
-  --nodes:Event /tmp/basecsv/Event.csv \
-  --nodes:Game /tmp/basecsv/Game.csv \
-  --nodes:Qualifier /tmp/basecsv/Qualifier.csv \
-  --nodes:Season /tmp/basecsv/Season.csv \
-  --nodes:Sport /tmp/basecsv/Sport.csv \
-  --relationships:HAPPENED_IN /tmp/basecsv/HAPPENED_IN.csv \
-  --relationships:IS_A_GAME_IN /tmp/basecsv/IS_A_GAME_IN.csv \
-  --relationships:IS_A_LEAGUE_IN /tmp/basecsv/IS_A_LEAGUE_IN.csv \
-  --relationships:IS_A_SEASON_OF /tmp/basecsv/IS_A_SEASON_OF.csv \
-  --relationships:IS_PART_OF /tmp/basecsv/IS_PART_OF.csv \
-  --relationships:PLAYED_IN /tmp/basecsv/PLAYED_IN.csv \
-  --relationships:PLAYS_FOR /tmp/basecsv/PLAYS_FOR.csv \
-  --relationships:PLAYS_IN /tmp/basecsv/PLAYS_IN.csv \
-  --relationships:WAS_INVOLVED_IN /tmp/basecsv/WAS_INVOLVED_IN.csv
+  --nodes:Player /tmp/csvgraph/Player.csv \
+  --nodes:Team /tmp/csvgraph/Team.csv \
+  --nodes:Competition /tmp/csvgraph/Competition.csv \
+  --nodes:Event /tmp/csvgraph/Event.csv \
+  --nodes:Game /tmp/csvgraph/Game.csv \
+  --nodes:Qualifier /tmp/csvgraph/Qualifier.csv \
+  --nodes:Season /tmp/csvgraph/Season.csv \
+  --nodes:Sport /tmp/csvgraph/Sport.csv \
+  --relationships:HAPPENED_IN /tmp/csvgraph/HAPPENED_IN.csv \
+  --relationships:PRECEDED /tmp/csvgraph/PRECEDED.csv \
+  --relationships:IS_A_GAME_IN /tmp/csvgraph/IS_A_GAME_IN.csv \
+  --relationships:IS_A_LEAGUE_IN /tmp/csvgraph/IS_A_LEAGUE_IN.csv \
+  --relationships:IS_A_SEASON_OF /tmp/csvgraph/IS_A_SEASON_OF.csv \
+  --relationships:IS_PART_OF /tmp/csvgraph/IS_PART_OF.csv \
+  --relationships:PLAYED_IN /tmp/csvgraph/PLAYED_IN.csv \
+  --relationships:PLAYS_FOR /tmp/csvgraph/PLAYS_FOR.csv \
+  --relationships:PLAYS_IN /tmp/csvgraph/PLAYS_IN.csv \
+  --relationships:WAS_INVOLVED_IN /tmp/csvgraph/WAS_INVOLVED_IN.csv
