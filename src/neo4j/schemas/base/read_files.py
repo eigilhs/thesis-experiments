639806d124e10ca72333e910192fc90419062349
Eigil Skjæveland <eigil@skjaeve.land>
2017-07-18 13:45:29 +0200

    Don't load events from before 2015.

--- src/neo4j/schemas/base/read_files.py
+++ src/neo4j/schemas/base/read_files.py
@@ -65,6 +65,8 @@ for fname in glob.glob(os.path.join(datadir, 'srml-*-squads.xml')):
     print(' done.', file=sys.stderr)
 
 for fname in glob.glob(os.path.join(datadir, 'f24*eventdetails.xml')):
+    if int(fname.split('-')[2]) < 2015:
+        continue
     print('Reading', fname, '...', end='', file=sys.stderr, flush=True)
     games = et.parse(fname).getroot()
 
