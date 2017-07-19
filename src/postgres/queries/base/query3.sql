9f38efdc8f162fa39a1de726f8c2c9b7d92afebf
Eigil Skjæveland <eigil@skjaeve.land>
2017-07-19 18:28:01 +0200

    Fix base/query3.sql.

--- src/postgres/queries/base/query3.sql
+++ src/postgres/queries/base/query3.sql
@@ -1,26 +1,16 @@
-WITH ca_starts AS (SELECT * FROM
-     (SELECT *, lead(x) OVER () AS lx,
-                lead(min) OVER () AS lmin,
-                lead(sec) OVER () AS lsec,
-                lead(period_id) OVER () AS lep
+SELECT count(*)
+FROM (SELECT DISTINCT *, lead(x) OVER () AS lx,
+                         lead(min) OVER () AS lmin,
+                         lead(sec) OVER () AS lsec
       FROM (SELECT *
-            FROM (SELECT *,
-                  lead(period_id) OVER w AS lpi,
-                  lead(team_id) OVER w AS lti
+            FROM (SELECT *, lead(team_id) OVER w AS lti
                   FROM events
-                  WINDOW w AS (ORDER BY match_id, period_id, min, sec, timestamp)
+                  WINDOW w AS (PARTITION BY match_id, period_id
+                               ORDER BY min, sec, timestamp)
                   ) A
-            WHERE team_id != lti AND lti is not null AND period_id = lpi
+            WHERE team_id != lti
             ) B
      ) C
-     WHERE period_id = lep
-       AND lsec + lmin*60 - (sec + min*60) < 10
-       AND lx - (100-x) > 20
-       AND (lx - (100-x)) / (lsec + lmin*60 - (sec + min*60) + 1) > 5
-)
-SELECT (SELECT array_agg(type_id)
-          FROM events
-         WHERE match_id = c.match_id
-           AND period_id = c.period_id
-           AND min * 60 + sec BETWEEN c.min * 60 + c.sec AND c.lmin * 60 + c.lsec)
-FROM ca_starts c;
+WHERE lsec + lmin*60 - (sec + min*60) BETWEEN 3 AND 10
+  AND lx - (100-x) > 20
+  AND (lx - (100-x)) / (lsec + lmin*60 - (sec + min*60)) > 6;
