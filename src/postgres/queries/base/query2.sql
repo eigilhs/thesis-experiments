02e893b763142730f9f4c4e797d95578a27f4c20
Eigil Skjæveland <eigil@skjaeve.land>
2017-07-20 00:37:11 +0200

    Update SQL queries.

--- src/postgres/queries/base/query2.sql
+++ src/postgres/queries/base/query2.sql
@@ -1,36 +1,23 @@
-SELECT count(*)
-FROM (
-    SELECT
-        match_id,
-        nth_value(events.id,       1) OVER w AS id1,
-        nth_value(events.type_id,  1) OVER w AS etype1,
-        nth_value(qs.qualifier_id, 1) OVER w AS qtype1,
-        nth_value(qs.qualifier_id, 2) OVER w AS qtype2,
-        nth_value(events.type_id,  2) OVER w AS etype2,
-        nth_value(events.type_id,  3) OVER w AS etype3,
-        nth_value(events.type_id,  4) OVER w AS etype4,
-        nth_value(qs.qualifier_id, 4) OVER w AS qtype4,
-        nth_value(events.outcome, 2) OVER w AS oc2,
-        nth_value(events.outcome, 4) OVER w AS oc4
-    FROM events
-    LEFT JOIN (
-         SELECT event_id, qualifier_id
-         FROM   qualifiers
-         WHERE  qualifiers.qualifier_id = 6
-         OR     qualifiers.qualifier_id = 15
-    ) AS qs ON qs.event_id = events.id
-    WINDOW w AS (
-        PARTITION BY match_id, period_id
-        ORDER BY min, sec, timestamp
-        ROWS 3 PRECEDING
-    )
-) AS situations
-WHERE situations.etype1 = 1
-AND   situations.qtype1 = 6
-AND   ((situations.etype2 = 12 AND situations.qtype2 = 15 AND situations.oc2)
-        OR (    situations.etype2 = 44
-            AND situations.etype3 = 44
-            AND situations.etype4 = 12
-            AND situations.qtype4 = 15
-            AND situations.oc4)
-);
+WITH RECURSIVE ordered_events AS (
+     SELECT id, type_id, next_id
+     FROM (SELECT *, lead(id) OVER w AS next_id FROM events
+           WINDOW w AS (PARTITION BY match_id, period_id
+                        ORDER BY min, sec, timestamp)) e
+     LEFT JOIN (
+         SELECT event_id, qualifier_id FROM qualifiers
+         WHERE qualifier_id IN (6, 15)
+     ) q ON q.event_id = e.id
+     WHERE type_id = 44
+        OR (type_id = 1 AND qualifier_id = 6)
+        OR (type_id = 12 AND qualifier_id = 15 AND outcome)
+),
+rec AS (
+     SELECT * FROM ordered_events
+     WHERE type_id = 1
+   UNION
+     SELECT e.* FROM (SELECT * FROM ordered_events
+                      WHERE type_id IN (44, 12)) e, rec r
+     WHERE e.id = r.next_id
+)
+SELECT count(*) FROM rec
+WHERE type_id = 12;
--- src/postgres/queries/jsonb/query2.sql
+++ src/postgres/queries/jsonb/query2.sql
@@ -1,30 +1,19 @@
-SELECT count(*)
-FROM (
-    SELECT
-        match_id,
-        nth_value(events.id,       1) OVER w AS id1,
-        nth_value(events.type_id,  1) OVER w AS etype1,
-        nth_value(events.type_id,  2) OVER w AS etype2,
-        nth_value(events.type_id,  3) OVER w AS etype3,
-        nth_value(events.type_id,  4) OVER w AS etype4,
-        nth_value(events.qualifiers ? '6',  1) OVER w AS q1,
-        nth_value(events.qualifiers ? '15', 2) OVER w AS q2,
-        nth_value(events.qualifiers ? '15', 4) OVER w AS q4,
-        nth_value(events.outcome, 2) OVER w AS oc2,
-        nth_value(events.outcome, 4) OVER w AS oc4
-    FROM events
-    WINDOW w AS (
-        PARTITION BY match_id, period_id
-        ORDER BY min, sec, timestamp
-        ROWS 3 PRECEDING
-    )
-) AS situations
-WHERE situations.etype1 = 1
-AND   situations.q1
-AND   ((situations.etype2 = 12 AND situations.q2 AND situations.oc2)
-        OR (    situations.etype2 = 44
-            AND situations.etype3 = 44
-            AND situations.etype4 = 12
-            AND situations.q4
-            AND situations.oc4)
-);
+WITH RECURSIVE ordered_events AS (
+     SELECT id, type_id, next_id
+     FROM (SELECT *, lead(id) OVER w AS next_id FROM events
+           WINDOW w AS (PARTITION BY match_id, period_id
+                        ORDER BY min, sec, timestamp)) e
+     WHERE type_id = 44
+        OR (type_id = 1  AND qualifiers ? '6')
+        OR (type_id = 12 AND qualifiers ? '15' AND outcome)
+),
+rec AS (
+     SELECT * FROM ordered_events
+     WHERE type_id = 1
+   UNION
+     SELECT e.* FROM (SELECT * FROM ordered_events
+                      WHERE type_id IN (44, 12)) e, rec r
+     WHERE e.id = r.next_id
+)
+SELECT count(*) FROM rec
+WHERE type_id = 12;
