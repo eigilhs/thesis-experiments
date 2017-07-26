dee2386dd94c325bd6a6cc5ab32be7bab61afbf3
Eigil Skjæveland <eigil@skjaeve.land>
2017-07-27 01:19:17 +0200

    Make query3.sql smarter than the planner.
    When events_1 was a CTE, the planner insisted on doing merge joins on
    it which included sorting it every time.

--- src/postgres/queries/base/query3.sql
+++ src/postgres/queries/base/query3.sql
@@ -1,21 +1,20 @@
-WITH RECURSIVE events_1 AS (
-    SELECT *, lead(team_id) OVER () AS next_team,
-              lead(id)      OVER () AS next_id
-      FROM events
-), possessions AS (
-    SELECT e.id attack_id, *
-      FROM events_1 e
+SELECT $$(SELECT id, team_id, min, sec, x,
+                 lead(team_id) OVER () AS next_team,
+                 lead(id) OVER () AS next_id
+          FROM events)$$ events_1 \gset
+EXPLAIN (ANALYZE, COSTS, VERBOSE, BUFFERS, FORMAT JSON) WITH RECURSIVE possessions AS (
+    SELECT id attack_id, *
+      FROM :events_1 e
      WHERE team_id != next_team
   UNION
     SELECT p.attack_id, e.*
-      FROM events_1 e, possessions p
+      FROM :events_1 e, possessions p
      WHERE e.id = p.next_id
        AND (e.team_id = p.team_id OR p.id = p.attack_id)
 )
 SELECT count(DISTINCT p.attack_id)
-  FROM possessions p
-  JOIN events_1 e
-    ON e.id = p.attack_id
+  FROM :events_1 e, possessions p
+ WHERE e.id = p.attack_id
    AND (p.sec - e.sec + (p.min - e.min)*60 >= 3
         AND p.x - (100-e.x) > 20
         AND (p.x - (100-e.x)) /
